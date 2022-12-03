c6986c7d4be1f4381c092f150650fec5df4b7b5b
JessicaSai <114347011+JessicaSai@users.noreply.github.com>
2022-12-03 14:47:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import time
 import requests
+
 # Add a Background Image from My Computer
 import base64
 def add_bg_from_local(image_file):
@@ -17,30 +18,46 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
+
 add_bg_from_local('background4.jpg')
+
+
 # title & header
 '''
 # Text Summarization
 This front queries the NEWSDATA.IO [API](https://newsdata.io/api/1/news?apikey=pub_140092ed3f8c155dd1a72797f696bcf187cf2&language=en&category=sports&q=badminton)
 '''
+
 # input parameters
 with st.form(key='params_for_api'):
     # Date_from = st.date_input('Enter date : E.g 2022-11-11')
     Keyword = st.text_input('Enter keyword : E.g World Cup')
-    Topic = st.text_input('Enter topic : E.g Sports')
+    #Topic = st.text_input('Enter topic : E.g Sports')
+
     st.form_submit_button('Enter')
+
 params = dict(
     # date_from=Date_from,
     keywords=Keyword,
-    category=Topic
+    #category=Topic
 )
+
 st.write(params)
+
 with st.spinner('Loading...'):
-    time.sleep(10)
-news_article_api_url = 'http://127.0.0.1:8000/predict'
-response = requests.get(news_article_api_url, params=params)
+    time.sleep(30)
+
+backend_url = 'http://127.0.0.1:8000/predict'
+response = requests.get(backend_url, params=params)
 print(response.status_code)
 prediction = response.json()
-for item in prediction['results']:
-    if item['content'] != None :
-        st.caption(item['content'])
+
+st.text_area('Sports Article', height=250, value=prediction['article'])
+
+col1, col2 = st.columns(2)
+
+with col1:
+   st.text_area("Base Model Summary", height=250, value=prediction['summary'])
+
+with col2:
+   st.text_area("Finetuned Model Summary", height=250, value=prediction['summary2'])
